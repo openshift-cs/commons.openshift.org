ebc12b84f56731635ad6af893801e5104ce6f622
Robert Bottomley <rbottomley@gmail.com>
2022-02-04 10:43:27 -0800

    Add filtering of participants

--- src/pages/participants.js
+++ src/pages/participants.js
@@ -8,13 +8,7 @@ const LogoImage = ({ image, innerClassName, ...other }) => {
   if (image?.extension === 'svg')
     return (
       <div>
-        <img
-          className="max-h-20"
-          width="128"
-          height="80"
-          src={image.publicURL}
-          alt=""
-        />
+        <img className="max-h-20" width="128" height="80" src={image.publicURL} alt="" />
       </div>
     )
 
@@ -31,8 +25,20 @@ const Participants = ({ onClick }) => {
   const [filterInput, setFilterInput] = useState('')
 
   const filterQuery = (evt) => {
+    setFilterInput(evt.target.value)
+  }
+
+  const [typeInput, setTypeInput] = useState('')
+
+  const typeQuery = (evt) => {
     const query = evt.target.value
-    setFilterInput(query)
+    setTypeInput(evt.target.value)
+  }
+
+  const [subtypeInput, setSubtypeInput] = useState('')
+
+  const subtypeQuery = (evt) => {
+    setSubtypeInput(evt.target.value)
   }
 
   return (
@@ -60,42 +66,84 @@ const Participants = ({ onClick }) => {
       `}
       render={(data) => (
         <>
-          <div className="relative block lg:!w-[410px]">
-            <div className="flex w-full justify-center items-center">
-              <label
-                className="pl-2 w-6 h-6 absolute transform left-0 pointer-events-none"
-                htmlFor="filterbox"
-              >
-                <SearchIcon
-                  alt=""
-                  className="text-base-600 absolute w-full h-full"
-                />
-                <span className="sr-only">Search participans</span>
-              </label>
-              <div className="shadow-underline text-base-400 w-full">
-                <input
-                  id="filterbox"
-                  type="search"
-                  className="form-input bg-base-100 dark:bg-base-0 flex pl-10 w-full placeholder-base-500 focus:bg-base-200 border-0 py-2 border-base-400 hover:border-base-500 focus:border-base-500 pr-2 h-9 md:h-10 lg:h-11 text-base-700 rounded-t-sm hover:rounded-sm hover:ring-2 hover:ring-base-500 focus:ring-2 focus:ring-primary-600 focus:rounded-sm"
-                  value={filterInput}
-                  onFocus={filterQuery}
-                  onChange={filterQuery}
-                  placeholder="Search participants"
-                />
+          <div className="relative">
+            <div className="flex flex-wrap gap-4">
+              <div className="lg:!w-[410px] flex w-full justify-center items-center">
+                <label
+                  className="pl-2 w-6 h-6 absolute transform left-0 pointer-events-none"
+                  htmlFor="filterbox"
+                >
+                  <SearchIcon alt="" className="text-base-600 absolute w-full h-full" />
+                  <span className="sr-only">Search participans</span>
+                </label>
+                <div className="shadow-underline text-base-400 w-full">
+                  <input
+                    id="filterbox"
+                    type="search"
+                    className="form-input bg-base-100 dark:bg-base-0 flex pl-10 w-full placeholder-base-500 focus:bg-base-200 border-0 py-2 border-base-400 hover:border-base-500 focus:border-base-500 pr-2 h-9 md:h-10 lg:h-11 text-base-700 rounded-t-sm hover:rounded-sm hover:ring-2 hover:ring-base-500 focus:ring-2 focus:ring-primary-600 focus:rounded-sm"
+                    value={filterInput}
+                    onFocus={filterQuery}
+                    onChange={filterQuery}
+                    placeholder="Search participants"
+                  />
+                </div>
               </div>
+              <div>
+                <label for="filtertype" className="">
+                  <span className="sr-only">Participant Type</span>
+                </label>
+                <select
+                  id="filtertype"
+                  className="rounded border-2 border-base-400 bg-base-100 dark:bg-base-0 text-base-500 hover:border-base-500 focus:text-base-700 focus:border-primary-600"
+                  defaultValue={typeInput}
+                  onFocus={typeQuery}
+                  onChange={typeQuery}
+                >
+                  <option value="">— Participant Type —</option>
+                  <option value="all">All</option>
+                  <option value="enduser">End User</option>
+                  <option value="partner">Partner</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              {typeInput === 'partner' && (
+                <div>
+                  <label for="filtersubtype" className="">
+                    <span className="sr-only">Subtype</span>
+                  </label>
+                  <select
+                    id="filtersubtype"
+                    className="rounded border-2 border-base-400 bg-base-100 dark:bg-base-0 text-base-500 hover:border-base-500 focus:text-base-700 focus:border-primary-600"
+                    defaultValue={subtypeInput}
+                    onFocus={subtypeQuery}
+                    onChange={subtypeQuery}
+                  >
+                    <option value="">— Subtype —</option>
+                    <option value="all">All</option>
+                    <option value="Solution Provider">Solution Provider</option>
+                    <option value="Service Cloud Provider">Service/Cloud Provider</option>
+                    <option value="Distributor">Distributor</option>
+                    <option value="ISV">ISV</option>
+                    <option value="OpenShift">OpenShift</option>
+                    <option value="OpenStack">OpenStack</option>
+                  </select>
+                </div>
+              )}
             </div>
           </div>
 
           <ul className="flex flex-wrap items-center gap-4 md:gap-8">
             {data.mdx.frontmatter.participants
+              .filter((p) => typeInput === '' || typeInput === 'all' || p.metatag1 === typeInput)
               .filter(
                 (p) =>
-                  p.name.toUpperCase().indexOf(filterInput.toUpperCase()) !==
-                  -1,
+                  typeInput !== 'partner' ||
+                  subtypeInput === '' ||
+                  subtypeInput === 'all' ||
+                  p.metatag2 === subtypeInput,
               )
-              .sort((a, b) =>
-                a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1,
-              )
+              .filter((p) => p.name.toUpperCase().indexOf(filterInput.toUpperCase()) !== -1)
+              .sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1))
               .map((p) => (
                 <li
                   key={p.name}
@@ -122,8 +170,7 @@ const Participants = ({ onClick }) => {
 }
 
 export default function ParticipantsPage() {
-  const headingStyle =
-    'font-headings text-3xl lg:text-5xl text-base-800 mb-2 lg:mb-4'
+  const headingStyle = 'font-headings text-3xl lg:text-5xl text-base-800 mb-2 lg:mb-4'
   const ledeStyle = 'max-w-[60ch] text-lg lg:text-xl text-base-600 mb-6'
 
   return (
@@ -133,9 +180,7 @@ export default function ParticipantsPage() {
         <div className="flex flex-col items-center space-y-32 xxxpage-wrapper">
           <section className="flex flex-col items-center space-y-8 w-full relative z-0">
             <h1 className={headingStyle}>OpenShift Commons Participants</h1>
-            <p className={ledeStyle}>
-              A community of collaborators working together.
-            </p>
+            <p className={ledeStyle}>A community of collaborators working together.</p>
             <Participants />
             <div
               className="absolute w-full top-0 left-0 h-full pointer-events-none z-[-1]"
