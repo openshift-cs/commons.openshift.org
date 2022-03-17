5b9cb82623bd9237ce56aa02d4d60cf82e15167a
Robert Bottomley <rbottomley@gmail.com>
2022-03-17 15:32:53 -0700

    Clean up classes

--- src/components/callout.js
+++ src/components/callout.js
@@ -12,7 +12,7 @@ const Callout = ({ ...props }) => {
           ? '_blank'
           : '_self'
       }
-      className={`group flex lg:h-full flex-col space-y-2 min-h-[112px] ring-2 ring-primary-600 ring-inset ring-opacity-10 hover:ring-opacity-50 focus:ring-opacity-100 focus:outline-none focus:ring-outset bg-gradient-to-br ${
+      className={`group flex lg:h-full flex-col gap-2 min-h-[112px] ring-2 ring-primary-600 ring-inset ring-opacity-10 hover:ring-opacity-50 focus:ring-opacity-100 focus:outline-none focus:ring-outset bg-gradient-to-br ${
         type === 'tertiary' &&
         'from-tertiary-200 to-success-200 hover:from-tertiary-300 hover:to-success-300 focus:from-tertiary-300 focus:to-success-300'
       }
@@ -21,7 +21,7 @@ const Callout = ({ ...props }) => {
         'from-alert-200 to-warning-200 dark:from-primary-200 dark:to-accent-200 hover:from-alert-300 hover:to-warning-300 focus:from-alert-300 focus:to-warning-300 dark:hover:from-primary-300 dark:hover:to-accent-300 dark:focus:from-primary-300 dark:focus:to-accent-300'
       } rounded-lg p-4 ${className}`}
     >
-      <div className="flex space-x-3 items-center">
+      <div className="flex gap-3 items-center">
         <div className="h-6 w-6 text-secondary-800 flex items-center">
           {icon}
         </div>
