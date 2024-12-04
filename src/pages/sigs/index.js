6810e20d3a559152baebf7b0d7eb8598939e1dae
Amy Marrich <spotzz@users.noreply.github.com>
2024-12-04 14:34:18 -0600

    Revert "Remove links to virtualgatherings"

--- src/pages/sigs/index.js
+++ src/pages/sigs/index.js
@@ -559,7 +559,7 @@ export default function SIGPage() {
               </AccordionItem>
             </Accordion.Root>
             <p className="mt-6 mb-2 text-center">
-              <IconBox title="Ready to join?" url={slack} icon={<Slack alt="" />} />
+              <IconBox title="Ready to join?" url="/virtualgatherings/" />
             </p>
             <div
               className="absolute w-full top-0 left-0 h-full pointer-events-none z-[-1]"
